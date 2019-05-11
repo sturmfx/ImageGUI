72b42dc460aa4b1e082e53c10461ff8c57c55f3c
lordstorm <ttunikitashtobert@gmail.com>
2019-05-11 12:42:59 +0300

    updated frame size

--- src/imagegui/ImageGUI.java
+++ src/imagegui/ImageGUI.java
@@ -19,6 +19,7 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javax.imageio.ImageIO;
 
@@ -28,11 +29,25 @@ import javax.imageio.ImageIO;
  */
 public class ImageGUI extends Application 
 {
+    int number_of_circle = 3;
+    int circles_min_diam = 50;
+    int circles_max_diam = 200;
+    int circles_brush = 10;
+    
+    int number_of_rect = 3;
+    int rectangle_min_x = 100;
+    int rectangle_max_x = 200;
+    int rectangle_min_y = 100;
+    int rectangle_max_y = 200;
+    int rectangle_brush = 10;
+    
+    String file_name = "";
+    final FileChooser fileChooser = new FileChooser();
     ImageManipulation im = new ImageManipulation();
     @Override
     public void start(Stage primaryStage) 
     {
-        ImageManipulation.load_from(im.directory2);
+        //ImageManipulation.load_from();
         Canvas can = new Canvas(ImageManipulation.x, ImageManipulation.y);
         
         Button btn1 = new Button();
@@ -45,7 +60,7 @@ public class ImageGUI extends Application
             {
                 GraphicsContext gc = can.getGraphicsContext2D();
                 ImageManipulation.noise(4);
-                Image image = SwingFXUtils.toFXImage(ImageManipulation.image1, null);
+                Image image = SwingFXUtils.toFXImage(ImageManipulation.images.get(ImageManipulation.id), null);
                 gc.drawImage(image, 0, 0);
             }
         });
@@ -59,28 +74,118 @@ public class ImageGUI extends Application
             public void handle(ActionEvent event) 
             {
                 GraphicsContext gc = can.getGraphicsContext2D();
-                ImageManipulation.load_from(im.directory2);
-                Image image = SwingFXUtils.toFXImage(ImageManipulation.image1, null);
+                
+                Image image = SwingFXUtils.toFXImage(ImageManipulation.images.get(0), null);
                 gc.drawImage(image, 0, 0);
             }
         });
         
         Button btn3 = new Button();
-        btn3.setText("SAVE");
+        btn3.setText("RECTANGLES");
         btn3.setOnAction(new EventHandler<ActionEvent>() 
         {
             
             @Override
             public void handle(ActionEvent event) 
             {
-                WritableImage wim = new WritableImage(ImageManipulation.x, ImageManipulation.y);
-                can.snapshot(null, wim);
-                try 
+                GraphicsContext gc = can.getGraphicsContext2D();
+                
+               
+                ImageManipulation.add_random_rectangles(number_of_rect, rectangle_min_x, rectangle_max_x, rectangle_min_y, rectangle_max_y, rectangle_brush);
+                Image image = SwingFXUtils.toFXImage(ImageManipulation.images.get(ImageManipulation.id), null);
+                gc.drawImage(image, 0, 0);
+            }
+        });
+        
+        Button btn4 = new Button();
+        btn4.setText("CIRCLES");
+        btn4.setOnAction(new EventHandler<ActionEvent>() 
+        {
+            
+            @Override
+            public void handle(ActionEvent event) 
+            {
+                GraphicsContext gc = can.getGraphicsContext2D();
+                
+               
+                ImageManipulation.add_random_circles(number_of_circle, circles_min_diam, circles_max_diam, circles_brush);
+                Image image = SwingFXUtils.toFXImage(ImageManipulation.images.get(ImageManipulation.id), null);
+                gc.drawImage(image, 0, 0);
+            }
+        });
+        Button choose_file = new Button();
+        choose_file.setText("LOAD IMAGE");
+        choose_file.setOnAction(
+            new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(final ActionEvent e) {
+                    File file = fileChooser.showOpenDialog(primaryStage);
+                    if (file != null) 
+                    {
+                        GraphicsContext gc = can.getGraphicsContext2D();
+                        ImageManipulation.directory = file.getAbsolutePath();
+                        ImageManipulation.load_from();
+                        Image image = SwingFXUtils.toFXImage(ImageManipulation.image1, null);
+                        gc.drawImage(image, 0, 0);
+                    }
+                }
+            });
+        
+        Button choose_file_save = new Button();
+        choose_file_save.setText("SAVE IMAGE");
+        choose_file_save.setOnAction(
+            new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(final ActionEvent e) 
                 {
-                    ImageIO.write(SwingFXUtils.fromFXImage(wim, null), "png", new File(im.directory));
-                } 
-                catch(Exception s)
+                    File file = fileChooser.showOpenDialog(primaryStage);
+                    if (file != null) 
+                    {
+                        WritableImage wim = new WritableImage(ImageManipulation.x, ImageManipulation.y);
+                        can.snapshot(null, wim);
+                        try 
+                        {
+                            ImageIO.write(SwingFXUtils.fromFXImage(wim, null), "png", file);
+                        } 
+                        catch(Exception s)
+                        {
+                        }
+                    }
+                }
+            });
+        
+        Button undo = new Button();
+        undo.setText("UNDO");
+        undo.setOnAction(new EventHandler<ActionEvent>() 
+        {
+            
+            @Override
+            public void handle(ActionEvent event) 
+            {
+                GraphicsContext gc = can.getGraphicsContext2D();
+                if(ImageManipulation.id>0)
                 {
+                    ImageManipulation.id--;
+                    Image image = SwingFXUtils.toFXImage(ImageManipulation.images.get(ImageManipulation.id), null);
+                    gc.drawImage(image, 0, 0);
+                }
+            }
+        });
+        
+        Button redo = new Button();
+        redo.setText("REDO");
+        redo.setOnAction(new EventHandler<ActionEvent>() 
+        {
+            
+            @Override
+            public void handle(ActionEvent event) 
+            {
+                GraphicsContext gc = can.getGraphicsContext2D();
+                if(ImageManipulation.id< ImageManipulation.images.size()-1)
+                {
+                    ImageManipulation.id++;
+                    Image image = SwingFXUtils.toFXImage(ImageManipulation.images.get(ImageManipulation.id), null);
+                    gc.drawImage(image, 0, 0);
                 }
             }
         });
@@ -89,9 +194,14 @@ public class ImageGUI extends Application
         VBox buttons = new VBox();
         root.getChildren().add(can);
         root.getChildren().add(buttons);
+        buttons.getChildren().add(choose_file);
+        buttons.getChildren().add(choose_file_save);
         buttons.getChildren().add(btn2);
         buttons.getChildren().add(btn1);
         buttons.getChildren().add(btn3);
+        buttons.getChildren().add(btn4);
+        buttons.getChildren().add(undo);
+        buttons.getChildren().add(redo);
         
         Scene scene = new Scene(root, 1100, 1000);
         
--- src/imagegui/ImageManipulation.java
+++ src/imagegui/ImageManipulation.java
@@ -9,8 +9,11 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -26,11 +29,14 @@ public class ImageManipulation
     /**
      * @param args the command line arguments
      */
+    public static int id = 0;
+    public static String file_name = "";
     public static int x = 1000;
     public static int y = 1000;
-    String directory = "C:\\Users\\lordstorm\\Pictures\\forest2.png";
-    String directory2 = "C:\\Users\\lordstorm\\Pictures\\forest1.jpg";
+    static String directory = "C:\\Users\\lordstorm\\Pictures\\forest2.png";
+    static String directory2 = "C:\\Users\\lordstorm\\Pictures\\forest1.jpg";
     public static BufferedImage image1 = new BufferedImage(x,y,BufferedImage.TYPE_INT_RGB);
+    public static ArrayList<BufferedImage> images = new ArrayList<BufferedImage>();
     static Color[] color_list = {Color.BLACK, Color.RED, Color.GREEN, Color.YELLOW, Color.BLUE};
     public ImageManipulation()
     {
@@ -38,9 +44,15 @@ public class ImageManipulation
     
     public static void save_to(String dir)
     {
+        int k = 0;
+        k = directory2.length();
+        StringBuilder b = new StringBuilder(directory2);
+        b.setCharAt(k-5,'t');
+        directory2 = b.toString();
+        
         try 
         {
-            ImageIO.write(ImageManipulation.image1, "jpg",new File(dir));
+            ImageIO.write(ImageManipulation.image1, "png",new File(directory2));
         } 
         catch (IOException ex) 
         {
@@ -48,11 +60,12 @@ public class ImageManipulation
         }
     }
     
-    public static void load_from(String dir)
+    public static void load_from()
     {
         try 
         {
-            ImageManipulation.image1 = ImageIO.read(new File(dir));
+            ImageManipulation.image1 = ImageIO.read(new File(directory));
+            images.add(deepCopy(image1));
         } 
         catch (IOException ex) 
         {
@@ -62,14 +75,16 @@ public class ImageManipulation
     
     public static void add_random_circles(int n, int min_diam, int max_diam, int brush_width)
     {
-        Graphics2D g2d = ImageManipulation.image1.createGraphics();
+        images.add(deepCopy(images.get(id)));
+        id++;
+        Graphics2D g2d = images.get(id).createGraphics();
         Random r = new Random();
         for(int i = 0; i < n; i++)
         {
             g2d.setColor(color_list[r.nextInt(5)]);
             int d = min_diam + r.nextInt(max_diam-min_diam);
-            int w = r.nextInt(ImageManipulation.image1.getWidth());
-            int h = r.nextInt(ImageManipulation.image1.getHeight());
+            int w = r.nextInt(images.get(id).getWidth());
+            int h = r.nextInt(images.get(id).getHeight());
             g2d.fillOval(w, h, d, d);
             g2d.setColor(Color.white);
             g2d.fillOval(w + brush_width, h + brush_width, d - 2*brush_width, d - 2*brush_width);
@@ -79,15 +94,17 @@ public class ImageManipulation
     
     public static void add_random_rectangles(int n, int min_w, int max_w, int min_h, int max_h,  int brush_width)
     {
-        Graphics2D g2d = ImageManipulation.image1.createGraphics();
+        images.add(deepCopy(images.get(id)));
+        id++;
+        Graphics2D g2d = images.get(id).createGraphics();
         Random r = new Random();
         for(int i = 0; i < n; i++)
         {
             g2d.setColor(color_list[r.nextInt(5)]);
             int dw = min_w + r.nextInt(max_w-min_w);
             int dh = min_h + r.nextInt(max_h - min_h);
-            int w = r.nextInt(ImageManipulation.image1.getWidth());
-            int h = r.nextInt(ImageManipulation.image1.getHeight());
+            int w = r.nextInt(images.get(id).getWidth());
+            int h = r.nextInt(images.get(id).getHeight());
             g2d.fillRect(w, h, dw, dh);
             g2d.setColor(Color.white);
             g2d.fillRect(w + brush_width, h + brush_width, dw - 2*brush_width, dh - 2*brush_width);
@@ -97,18 +114,20 @@ public class ImageManipulation
     
     public static void noise(int foreach)
     {
+        images.add(deepCopy(images.get(id)));
+        id++;
         int n = 0;
         int m = 0;
         Random r = new Random();
-        for(int i = 0; i < ImageManipulation.image1.getWidth(); i++)
+        for(int i = 0; i < images.get(id).getWidth(); i++)
         {
-            for(int j = 0; j < ImageManipulation.image1.getHeight(); j++)
+            for(int j = 0; j < images.get(id).getHeight(); j++)
             {
                 n = r.nextInt();
                 m = color_list[r.nextInt(5)].getRGB();
                 if(n % foreach == 0)
                 {
-                    ImageManipulation.image1.setRGB(i, j, m);
+                    images.get(id).setRGB(i, j, m);
                 }
             }
         }
@@ -116,6 +135,7 @@ public class ImageManipulation
     
     public static void noise1(int x1, int y1, int dx, int dy, Color color)
     {
+        id++;
         int n = 0;
         int m = color.getRGB();
         Random r = new Random();
@@ -131,5 +151,22 @@ public class ImageManipulation
                 }
             }
         }
+    }
+    
+    public static void load_previous()
+    {
+        if(id>1)
+        {
+        }
+    }
+    
+    
+
+    static BufferedImage deepCopy(BufferedImage bi)
+    {
+        ColorModel cm = bi.getColorModel();
+        boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+        WritableRaster raster = bi.copyData(null);
+        return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
     }
 }
